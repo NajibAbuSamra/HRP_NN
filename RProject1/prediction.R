848b140db2712334ae692eff1d2531730da9abe8
Najib <najib.as1990@gmail.com>
2018-07-11 21:08:14 +0300

    removed comments

--- RProject1/prediction.R
+++ RProject1/prediction.R
@@ -17,26 +17,12 @@ data = read.csv(args[1], header = T)
 ## Scale data for neural network
 data <- data[, sapply(data, is.numeric)]
 
-#### MAY NOT NEED TO SCALE
-#max <- apply(data, 2, max)
-#min <- apply(data, 2, min)
-#scaled = as.data.frame(scale(data, center = min, scale = max - min))
-
-
 # load library
 library(neuralnet)
 
-
-#Test the resulting output
-#temp_test <- subset(testset, select = c("fcfps", "earnings_growth", "de", "mcap", "current_ratio"))
-#head(temp_test)
-#nn.results <- compute(nn, temp_test)
 scaled <- subset(data, select = c("Demand", "Unit_Demand"))
 
 NN.results <- compute(NN, scaled)
-
-# plot neural network
-#plot(NN)
 
 #save neural network
 save(NN, file = 'c:/gitdownloads/hrp_nn/rproject1/network')
